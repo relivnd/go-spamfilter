f916434846b5802c1931388a94f9f1057ef6f684
Tobias Vonesch <tvonesch@tecracer.ch>
2024-12-12 14:10:28 +0100

    feat(): count all words and put them into a map

--- main.go
+++ main.go
@@ -5,16 +5,41 @@ import (
 	"spam_filter/utils"
 )
 
-const filesDir string = "files/"
-const hamDir string = filesDir + "ham-anlern/"
-const spamDir string = filesDir + "spam-anlern/"
+const (
+	filesDir string = "files/"
+	hamDir   string = filesDir + "ham-anlern/"
+	spamDir  string = filesDir + "spam-anlern/"
+)
+
+type Word struct {
+	spamOccurrences int16
+	hamOccurrences  int16
+}
 
 func main() {
 	fmt.Println("let's get all the provided ham files from the ham directory")
-	hamFilesNameSlice := utils.ListFilesInDir(hamDir)
-	fmt.Printf("there are %d ham files in the directory", len(hamFilesNameSlice))
-}
+	hamFiles := utils.ListFilesInDir(hamDir)
+	fmt.Printf("there are %d ham files in the directory", len(hamFiles))
+	wordOccurrences := make(map[string]Word)
+	for i := range hamFiles {
+		wordSlice := utils.TurnFileIntoStringSlice(hamDir + hamFiles[i])
+		for _, word := range wordSlice {
+			curr := wordOccurrences[word]
+			curr.hamOccurrences++
+			wordOccurrences[word] = curr
+		}
+	}
 
-type word struct{
-	probability float32
+	fmt.Println("let's get all the provided spam files from the spam directory")
+	spamFiles := utils.ListFilesInDir(spamDir)
+	fmt.Printf("there are %d spam files in the directory", len(spamFiles))
+	for i := range spamFiles {
+		wordSlice := utils.TurnFileIntoStringSlice(spamDir + spamFiles[i])
+		for _, word := range wordSlice {
+			curr := wordOccurrences[word]
+			curr.spamOccurrences++
+			wordOccurrences[word] = curr
+		}
+	}
+	fmt.Println(wordOccurrences)
 }
