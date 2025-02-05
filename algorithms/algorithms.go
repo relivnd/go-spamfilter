43112a0c40860518c22a0698cdb57088dfc250b9
relivnd <hej@relivnd.dev>
2025-02-05 10:01:22 +0100

    wip(): add algorithms file

--- algorithms/algorithms.go
+++ algorithms/algorithms.go
@@ -1,1 +1,17 @@
 package algorithms
+
+import (
+	"spam_filter/models"
+	"spam_filter/utils"
+)
+
+func ASDF(filenames []string, wordOccurrences *map[string]models.Word, directory string) {
+	for _, val := range filenames {
+		wordMap := utils.TurnFileIntoStringMap(directory + val)
+		for word := range wordMap {
+			curr := (*wordOccurrences)[word]
+			curr.HamOccurrences++
+			(*wordOccurrences)[word] = curr
+		}
+	}
+}
